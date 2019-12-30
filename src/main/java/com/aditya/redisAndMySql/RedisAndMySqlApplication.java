733249c66fed03a6766a55b26fadae0a4b5be7fb
aditya <aditya.rajiv@blackbuck.com>
2019-12-30 21:58:32 +0530

    project starting

---
+++ src/main/java/com/aditya/redisAndMySql/RedisAndMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.redisAndMySql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisAndMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisAndMySqlApplication.class, args);
+	}
+
+}
